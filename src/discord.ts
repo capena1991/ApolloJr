ba1cb7ddefb2f45804228f5ad9b0c519700986e9
Carlos Antonio Peña Ramón <capena1991@gmail.com>
2020-02-23 15:38:46 -0500

    added some basic useful commands

--- src/discord.ts
+++ src/discord.ts
@@ -8,9 +8,27 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on("message", ({ content, channel }) => {
-  if (content === `${prefix}ping`) {
+const getUserInfo = (user: Discord.User) =>
+  [`${user.username}'s ID: ${user.id}`, `${user.username}'s avatar: ${user.displayAvatarURL}`].join("\n")
+
+client.on("message", ({ content, channel, guild, author, mentions }) => {
+  if (!content.startsWith(prefix) || author.bot) {
+    return
+  }
+
+  const args = content.slice(prefix.length).split(/\s+/)
+  const command = args.shift()?.toLowerCase()
+
+  if (!command) {
+    channel.send("You talkin' to me?")
+  } else if (command === "ping") {
     channel.send("Pong.")
+  } else if (command === "server") {
+    channel.send(`Server name: ${guild.name}\nTotal members: ${guild.memberCount}`)
+  } else if (command === "user") {
+    const users = mentions.users.size ? mentions.users.array() : [author]
+    const message = users.map((u) => getUserInfo(u)).join("\n\n")
+    channel.send(message)
   }
 })
 
