2e4d8eac6e15181455196491cf58398a94aead10
Carlos Antonio Peña Ramón <capena1991@gmail.com>
2020-02-23 13:57:32 -0500

    added basic ping

--- src/discord.ts
+++ src/discord.ts
@@ -8,4 +8,10 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
+client.on("message", ({ content, channel }) => {
+  if (content === "!ping") {
+    channel.send("Pong.")
+  }
+})
+
 client.login(auth.token)
