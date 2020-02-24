f88e22d360f19569bc3de87fd3a7b3ac830aee09
Carlos Antonio Peña Ramón <capena1991@gmail.com>
2020-02-23 19:42:38 -0500

    added embed for user

--- src/commands/user.ts
+++ src/commands/user.ts
@@ -1,17 +1,38 @@
 import Discord from "discord.js"
+import moment from "moment"
 
 import { Command } from "./types"
 
-const getUserInfo = (user: Discord.User) =>
-  [`${user.username}'s ID: ${user.id}`, `${user.username}'s avatar: ${user.displayAvatarURL}`].join("\n")
+const getLastMessageTime = (isAuthor: boolean, lastMessage: Discord.Message) => {
+  if (isAuthor) {
+    return "right now... duh"
+  } else if (!lastMessage) {
+    return "¯\\_(ツ)_/¯"
+  } else {
+    return moment(lastMessage.createdAt).fromNow()
+  }
+}
+
+const getUserInfo = (
+  { id, username, displayAvatarURL, bot, createdAt, discriminator, lastMessage }: Discord.User,
+  isAuthor = false,
+) =>
+  new Discord.RichEmbed()
+    .setTitle("User Info")
+    .setDescription(`**<@${id}>** (${username}#${discriminator})${bot ? " :robot:" : ""}`)
+    .setThumbnail(displayAvatarURL)
+    .addField("ID", id, true)
+    .addField("Created", `${moment(createdAt).format("ll")}\n(${moment(createdAt).fromNow()})`, true)
+    .addField("Last message", getLastMessageTime(isAuthor, lastMessage), true)
 
 const ping: Command = {
   name: "user",
   description: "Display info about yourself or another user.",
   execute({ channel, mentions, author }) {
     const users = mentions.users.size ? mentions.users.array() : [author]
-    const message = users.map((u) => getUserInfo(u)).join("\n\n")
-    channel.send(message)
+    users.forEach((u) => {
+      channel.send(getUserInfo(u, u.id === author.id))
+    })
   },
 }
 
