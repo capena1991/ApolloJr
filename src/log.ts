9586e3ea113e394b6c1c40a8587bf090b0a68e4b
Carlos Antonio Peña Ramón <capena1991@gmail.com>
2020-02-25 00:35:29 -0500

    slightly improved logging for embeds

--- src/log.ts
+++ src/log.ts
@@ -2,7 +2,7 @@ import Discord from "discord.js"
 
 const logsChannelId = "681709479921975321"
 
-export const log = ({ client, channel, guild, author, content }: Discord.Message) => {
+export const log = ({ client, channel, guild, author, cleanContent, embeds }: Discord.Message) => {
   if (channel.id === logsChannelId) {
     return
   }
@@ -10,5 +10,6 @@ export const log = ({ client, channel, guild, author, content }: Discord.Message
   const channelStr = ["dm", "group"].includes(channel.type)
     ? "DM"
     : `${guild.name}#${(<Discord.GuildChannel>channel).name}`
+  const content = embeds.length ? (cleanContent ? `_<embed(s)>_ + ${cleanContent}` : "_<embed(s)>_") : cleanContent
   logChannel.send(`\`${channelStr}:${author.tag}\` ${content}`)
 }
