d995332135d85ff61d07ff399859178d2d91576f
Carlos Antonio Peña Ramón <capena1991@gmail.com>
2020-02-25 13:47:11 -0500

    caching logs channel

--- src/log.ts
+++ src/log.ts
@@ -1,12 +1,13 @@
 import Discord from "discord.js"
 
 const logsChannelId = "681709479921975321"
+let logChannel: Discord.TextChannel | undefined = undefined
 
 export const log = ({ client, channel, guild, author, cleanContent, embeds }: Discord.Message) => {
   if (channel.id === logsChannelId) {
     return
   }
-  const logChannel = <Discord.TextChannel>client.channels.get(logsChannelId)
+  logChannel = logChannel || <Discord.TextChannel>client.channels.get(logsChannelId)
   const channelStr = ["dm", "group"].includes(channel.type)
     ? "DM"
     : `${guild.name}#${(<Discord.GuildChannel>channel).name}`
