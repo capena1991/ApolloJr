fa8d582c79911b2740744fa9f16c836a3f9e9fe9
Carlos Antonio Peña Ramón <capena1991@gmail.com>
2020-03-03 15:37:33 -0500

    added a couple of utilities

--- src/utils.ts
+++ src/utils.ts
@@ -11,3 +11,20 @@ export const randInt = (min: number, max: number | undefined = undefined) => {
 export const chooseOne = (values: any[]) => values[randInt(values.length)]
 
 export const randBool = (probability: number = 0.5) => Math.random() < probability
+
+export const joinReadable = (list: string[]) => {
+  const len = list.length
+  switch (len) {
+    case 0:
+      return ""
+    case 1:
+      return list[0]
+    default:
+      return `${list.slice(0, len - 1).join(", ")} and ${list[len - 1]}`
+  }
+}
+
+export const schedule = (fn: (...args: any[]) => any, time: number, ...args: any[]) => {
+  setImmediate(fn, ...args)
+  return setInterval(fn, time, ...args)
+}
