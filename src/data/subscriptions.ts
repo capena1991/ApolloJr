cfc10ed771aa352dc639410177ebcd5511c220da
Carlos Antonio Peña Ramón <capena1991@gmail.com>
2020-03-03 15:41:06 -0500

    added subscriptions data manager

---
+++ src/data/subscriptions.ts
@@ -0,0 +1,39 @@
+import Keyv from "keyv"
+
+import { DataManager, defaultDB } from "./dataManager"
+
+export interface Subscription {
+  subscribed: { user: string; lastNotification?: { reason: string; datetime: string } }[]
+}
+
+const initializeSubscriptionData = () => ({
+  subscribed: [],
+})
+
+const subscriptionsKeyv = new Keyv<Subscription>(defaultDB, { namespace: "subscriptions" })
+
+export const subscriptions = new DataManager<Subscription>(subscriptionsKeyv, initializeSubscriptionData)
+
+export const subscribe = async (subscription: string, userId: string) => {
+  const { subscribed } = await subscriptions.get(subscription)
+  if (subscribed.find(({ user }) => user === userId)) {
+    return false
+  }
+  subscriptions.set(subscription, { subscribed: [...subscribed, { user: userId }] })
+  return true
+}
+
+export const toggleSubscribe = async (subscription: string, userId: string) => {
+  const { subscribed } = await subscriptions.get(subscription)
+  const index = subscribed.findIndex(({ user }) => user === userId)
+  if (index >= 0) {
+    subscriptions.set(subscription, { subscribed: [...subscribed.slice(0, index), ...subscribed.slice(index + 1)] })
+    return false
+  }
+  subscriptions.set(subscription, { subscribed: [...subscribed, { user: userId }] })
+  return true
+}
+
+export const knownSubscriptions = {
+  birthday: "birthday",
+}
