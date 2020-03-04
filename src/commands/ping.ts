64c660d0c1c6555f4c66f27bddc62522101f659d
Carlos Antonio Peña Ramón <capena1991@gmail.com>
2020-03-03 20:05:56 -0500

    added latency check on ping

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -3,8 +3,18 @@ import { Command } from "./types"
 const ping: Command = {
   name: "ping",
   description: "Ping me to check whether I'm listening.",
-  execute: ({ channel }) => {
-    channel.send("Pong.")
+  execute: async ({ channel, client, createdTimestamp }) => {
+    const base = "I'm awake! I'm awake. Just resting my eyes for a bit."
+    let message = await channel.send(base)
+    if (Array.isArray(message)) {
+      message = message[0]
+    }
+    const botPing = message.createdTimestamp - createdTimestamp
+    message.edit(
+      base +
+        "\nIf you doubt me, check out these amazing ~~or not~~ reaction times:\n" +
+        `**My ping**: ${Math.round(botPing)}ms  **API ping**: ${Math.round(client.ping)}ms`,
+    )
   },
 }
 
