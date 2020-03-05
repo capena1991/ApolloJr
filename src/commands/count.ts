31135e2dde00e9fb3a8ae79843b239c38ac43e3c
Carlos Antonio Peña Ramón <capena1991@gmail.com>
2020-03-05 18:46:59 -0500

    added round winning and restarting

--- src/commands/count.ts
+++ src/commands/count.ts
@@ -1,11 +1,13 @@
 import { Command } from "./types"
 
-import { getCurrent, setCurrent } from "../data/countingData"
+import { getCurrent, setCurrent, archiveCurrent } from "../data/countingData"
 
 const count: Command = {
   name: "count",
   description: "The game of count. Two teams try to get teh count to either 100 or -100",
   execute: async (message, args) => {
+    const { channel } = message
+
     const number = parseInt(args[0])
     if (!Number.isFinite(number)) {
       return message.delete()
@@ -15,8 +17,21 @@ const count: Command = {
     if (diff !== 1 && diff !== -1) {
       return message.delete()
     }
-    setCurrent({ count: count + diff, ...rest })
-    return message.react("✅")
+    message.react("☑")
+    const newCount = count + diff
+    await setCurrent({ count: newCount, ...rest })
+    if (newCount !== 100 && newCount !== -100) {
+      return
+    }
+    const archiveProm = archiveCurrent()
+    await channel.send(`${newCount > 0 ? "Positives" : "Negatives"} win this round! :tada:`)
+    const newRoundNumber = await archiveProm
+    await channel.send(`Round ${newRoundNumber} starts now.`)
+    let zeroMsg = await channel.send("0")
+    if (Array.isArray(zeroMsg)) {
+      zeroMsg = zeroMsg[0]
+    }
+    return zeroMsg.react("☑")
   },
 }
 
--- src/data/countingData.ts
+++ src/data/countingData.ts
@@ -3,11 +3,12 @@ import Keyv from "keyv"
 import { DataManager, defaultDB } from "./dataManager"
 
 export interface CountingRound {
-  roundNumber?: number
+  roundNumber: number
   count: number
 }
 
-const initializeCountingRound = () => ({
+const initializeCountingRound = (roundNumber = 0) => ({
+  roundNumber,
   count: 0,
 })
 
@@ -17,3 +18,10 @@ export const counting = new DataManager<CountingRound>(usersKeyv, initializeCoun
 
 export const getCurrent = () => counting.get("current")
 export const setCurrent = (data: CountingRound) => counting.set("current", data)
+export const archiveCurrent = async () => {
+  const current = await getCurrent()
+  counting.set(current.roundNumber.toString(), current)
+  const newRoundNumber = current.roundNumber + 1
+  setCurrent(initializeCountingRound(newRoundNumber))
+  return newRoundNumber
+}
