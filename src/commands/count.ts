1878d142ce2038a4fe9cb5a5bd369060ab9d72f7
Carlos Antonio Peña Ramón <capena1991@gmail.com>
2020-03-05 23:36:17 -0500

    added "no two messages in a row" restriction

--- src/commands/count.ts
+++ src/commands/count.ts
@@ -1,4 +1,5 @@
 import { Command } from "./types"
+import moment from "moment"
 
 import { getCurrent, setCurrent, archiveCurrent } from "../data/countingData"
 
@@ -6,23 +7,43 @@ const count: Command = {
   name: "count",
   description: "The game of count. Two teams try to get teh count to either 100 or -100",
   execute: async (message, args) => {
-    const { channel } = message
+    const { channel, author } = message
+
+    const reject = (reason?: string) => {
+      message.delete()
+      if (reason) {
+        author.send(reason)
+      }
+    }
 
     const number = parseInt(args[0])
     if (!Number.isFinite(number)) {
-      return message.delete()
+      return reject(
+        "We mean business in the counting channel. Valid numbers. Yours was not.\n" +
+          "Leave the idle chit chat for another channel.",
+      )
     }
-    const { count, ...rest } = await getCurrent()
+    const { count, last, ...rest } = await getCurrent()
     const diff = number - count
     if (diff !== 1 && diff !== -1) {
-      return message.delete()
+      return reject(
+        `Try again, pal. Last count was **${count}**. :rolling_eyes:\n` +
+          "If I've said this to you too many times, maybe you should consider going back to elementary school. :wink:",
+      )
     }
+
+    if (last.user === author.id) {
+      return reject("Nice try :smirk:, but you gotta let others play. Can't count twice in a row.")
+    }
+
     message.react("☑")
     const newCount = count + diff
-    await setCurrent({ count: newCount, ...rest })
+    await setCurrent({ count: newCount, last: { user: author.id, datetime: moment().toISOString() }, ...rest })
+
     if (newCount !== 100 && newCount !== -100) {
       return
     }
+
     const archiveProm = archiveCurrent()
     await channel.send(`${newCount > 0 ? "Positives" : "Negatives"} win this round! :tada:`)
     const newRoundNumber = await archiveProm
--- src/data/countingData.ts
+++ src/data/countingData.ts
@@ -1,15 +1,18 @@
 import Keyv from "keyv"
+import moment from "moment"
 
 import { DataManager, defaultDB } from "./dataManager"
 
 export interface CountingRound {
   roundNumber: number
   count: number
+  last: { user: string; datetime: string }
 }
 
 const initializeCountingRound = (roundNumber = 0) => ({
   roundNumber,
   count: 0,
+  last: { user: "681209545539846185", datetime: moment().toISOString() }, // bot's user id
 })
 
 const usersKeyv = new Keyv<CountingRound>(defaultDB, { namespace: "counting" })
@@ -17,7 +20,9 @@ const usersKeyv = new Keyv<CountingRound>(defaultDB, { namespace: "counting" })
 export const counting = new DataManager<CountingRound>(usersKeyv, initializeCountingRound)
 
 export const getCurrent = () => counting.get("current")
+
 export const setCurrent = (data: CountingRound) => counting.set("current", data)
+
 export const archiveCurrent = async () => {
   const current = await getCurrent()
   counting.set(current.roundNumber.toString(), current)
