aee567dfe5bb057c978d29e31d98fc005697182a
Carlos Antonio Peña Ramón <capena1991@gmail.com>
2021-03-21 17:44:16 -0400

    Revert "very basic subcommand support"
    This reverts commit cf07f4d1b1501fa6bb654558f5e90a0a3ca42727.

--- src/commands/types.ts
+++ src/commands/types.ts
@@ -4,26 +4,5 @@ export interface Command {
   name: string
   aliases?: string[]
   description: string
-  subcommands?: Command[]
-  default?: boolean
   execute: (message: Discord.Message, args: string[]) => void
-}
-
-export const executeSubcommands = (subcommands: Command[]) => {
-  return (message: Discord.Message, args: string[]) => {
-    const cmd = args[0]
-    let defaultExec
-    for (let i = 0; i < subcommands.length; i++) {
-      const { name, aliases, execute, default: isDefault } = subcommands[i]
-      if ([name, ...(aliases || [])].includes(cmd)) {
-        return execute(message, args.slice(1))
-      }
-      if (isDefault) {
-        defaultExec = execute
-      }
-    }
-    if (defaultExec) {
-      defaultExec(message, args)
-    }
-  }
 }
