4380b42ec431c08ad820d36e6d1a67678c65e171
Carlos Antonio Peña Ramón <capena1991@gmail.com>
2023-07-06 00:09:45 -0400

    Do not react to own messages

--- src/commands/commands/keepAlive.ts
+++ src/commands/commands/keepAlive.ts
@@ -1,11 +1,15 @@
+import { botUserId } from "../../utilities/config"
 import { MessageCommand } from "../types"
 
 const keepAlive: MessageCommand = {
   name: "keepAlive",
   description: "React to ping to stay alive.",
   allowsBots: true,
-  runOnMessage: async (message) => {
-    await message.channel.send("ack")
+  runOnMessage: async ({ channel, author }) => {
+    if (author.id === botUserId) {
+      return
+    }
+    await channel.send("ack")
   },
 }
 
