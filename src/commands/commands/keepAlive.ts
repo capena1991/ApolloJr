f767f9e9743c2913dbc420d91c54c8e8c1fc7473
Carlos Antonio Peña Ramón <capena1991@gmail.com>
2023-07-06 00:34:40 -0400

    Use self ping instead

--- src/commands/commands/keepAlive.ts
+++ src/commands/commands/keepAlive.ts
@@ -1,15 +1,13 @@
-import { botUserId } from "../../utilities/config"
+import { sleep } from "../../utilities/utils"
 import { MessageCommand } from "../types"
 
 const keepAlive: MessageCommand = {
   name: "keepAlive",
   description: "React to ping to stay alive.",
   allowsBots: true,
-  runOnMessage: async ({ channel, author }) => {
-    if (author.id === botUserId) {
-      return
-    }
-    await channel.send("ack")
+  runOnMessage: async ({ channel }) => {
+    await sleep(60000)
+    await channel.send("ping")
   },
 }
 
